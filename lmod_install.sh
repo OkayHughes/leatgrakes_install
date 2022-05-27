4a969b6578f595fd5988978ad0b00b84655b58c1
Okay Hughes <okayhughes@gmail.com>
2022-05-27 19:54:43 -0400

    Update to install lua correctly

--- lmod_install.sh
+++ lmod_install.sh
@@ -1,3 +1,14 @@
+mkdir -p install \
+	&& wget -N --no-check-certificate https://sourceforge.net/projects/lmod/files/lua-5.1.4.9.tar.bz2 \
+	&& tar -xf lua-5.1.4.9.tar.bz2 \
+	&& cd lua-5.1.4.9 \
+	&& ./configure --prefix=/opt/apps/lua/5.1.4 \
+	&& make -j10 \
+	&& make -j10 install \
+	&& ln -sf /opt/apps/lua/5.1.4 /opt/apps/lua/lua \
+	&& mkdir -p /usr/local/bin \
+	&& ln -sf /opt/apps/lua/bin/lua /usr/local/bin
+
 sudo yum  install -y lua-5.1.4-15.el7.x86_64 \
 	lua-bitop-1.0.2-3.el7.x86_64 \
 	lua-devel-5.1.4-15.el7.x86_64 \
@@ -11,13 +22,13 @@ sudo yum  install -y lua-5.1.4-15.el7.x86_64 \
 
 mkdir -p /install/lmod \
 	&& cd /install/lmod \
-	&& wget --no-check-certificate https://sourceforge.net/projects/lmod/files/Lmod-8.7.tar.bz2 \
+	&& wget -N --no-check-certificate https://sourceforge.net/projects/lmod/files/Lmod-8.7.tar.bz2 \
 	&& tar -xf Lmod-8.7.tar.bz2 \
 	&& cd Lmod-8.7 \
 	&& ./configure --prefix=/opt/apps \
 	&& make -j10 install
 
-ln -s /opt/apps/lmod/lmod/init/profile        /etc/profile.d/z00_lmod.sh \
-	&& ln -s /opt/apps/lmod/lmod/init/cshrc          /etc/profile.d/z00_lmod.csh 
+ln -sf /opt/apps/lmod/lmod/init/profile        /etc/profile.d/z00_lmod.sh \
+	&& ln -sf /opt/apps/lmod/lmod/init/cshrc          /etc/profile.d/z00_lmod.csh 
 
 
