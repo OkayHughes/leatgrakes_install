8e3ebfbb4c2bc603c208b62e5d39feb72dccfef6
OkayHughes <okayhughes@gmail.com>
2022-05-29 15:32:39 -0400

    Install oneapi script created

---
+++ intel_install.sh
@@ -0,0 +1,19 @@
+tee > /tmp/oneAPI.repo << EOF
+[oneAPI]
+name=Intel® oneAPI repository
+baseurl=https://yum.repos.intel.com/oneapi
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://yum.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS.PUB
+EOF
+
+sudo mv /tmp/oneAPI.repo /etc/yum.repos.d
+
+sudo yum -y install cmake pkgconfig
+sudo yum -y groupinstall "Development Tools"
+
+
+sudo yum -y install intel-basekit
+sudo yum -y install intel-hpckit
+
